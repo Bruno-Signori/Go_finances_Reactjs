9a3a195341c6f02ecd93743d62d203693e0c5eb9
Bruno-Signori <brunowsignori@gmail.com>
2020-05-19 10:27:53 -0300

    desafio reactjs Gofinances

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,7 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
-
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(value);
+// funçaõ para formatar OS numeros dos valores que vem do backend no estilo de moeda;
 export default formatValue;
